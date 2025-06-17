2fc333282afb154cf588caead40473f6dfd32523
shiffananazeema <shiffananazeema@gmail.com>
2025-06-17 20:51:33 +1000

    Day 4

--- 1. Python/programs.py
+++ 1. Python/programs.py
@@ -1,25 +1,83 @@
-#Unit Converter:
-def unit_converter():
-    conversion={
-        'km': 1000,
-        'm': 1,
-        'cm': 0.01,
-        'mm': 0.001
-    }
+# Ctrl+/ -> Comment/Uncomment
 
-    print("Unit Conversion:")
-    print("Avaiblale Units: km, m, cm, mm")
-    value = float(input("Enter the value to convert: "))
-    from_unit = input("Convert from (mm/cm/m/km): ").lower()
-    to_unit = input("Convert to (mm/cm/m/km): ").lower()
+# # 1. Unit Converter:
+# def unit_converter():
+#     conversion={
+#         'km': 1000,
+#         'm': 1,
+#         'cm': 0.01,
+#         'mm': 0.001
+#     }
 
-    if from_unit not in conversion or to_unit not in conversion:
-        print("Invalid unit")
+#     print("Unit Conversion:")
+#     print("Avaiblale Units: km, m, cm, mm")
+#     value = float(input("Enter the value to convert: "))
+#     from_unit = input("Convert from (mm/cm/m/km): ").lower()
+#     to_unit = input("Convert to (mm/cm/m/km): ").lower()
 
-    else:
-        meters = value * conversion[from_unit]
-        converted_value = meters / conversion[to_unit]
+#     if from_unit not in conversion or to_unit not in conversion:
+#         print("Invalid unit")
 
-        print(f"{value} {from_unit} is {converted_value} {to_unit} ")
+#     else:
+#         meters = value * conversion[from_unit]
+#         converted_value = meters / conversion[to_unit]
 
-unit_converter()
+#         print(f"{value} {from_unit} is {converted_value} {to_unit} ")
+
+# unit_converter()
+
+# 2. Check for keywords
+
+# import keyword
+    
+# print("\nType '/' to exit the program")
+
+# while True:
+
+#     word = input("\nEnter the word: ")
+#     if word == '/':
+#         print("\nSee u soon")
+#         break
+#     elif keyword.iskeyword(word):
+#         print(f"\n'{word}' is a keyword")
+#     else:
+#         print(f"\n'{word}' is not a keyword")
+
+# 3. Calculator
+
+# fn = float(input("Enter first number: "))
+# sn = float(input("Enter second number: "))
+# op = input("Enter Operator(+,-,*,/,//,%,** or all): ")
+# if op == '+':
+#     result = fn + sn
+#     print(f"{fn} {op} {sn} = {result}")
+# elif op == "-":
+#     result = fn - sn
+#     print(f"{fn} {op} {sn} = {result}")
+# elif op == '*':
+#     result = fn * sn
+#     print(f"{fn} {op} {sn} = {result}")
+# elif op == "/":
+#     result = fn / sn
+#     print(f"{fn} {op} {sn} = {result}")
+# elif op == '//':
+#     result = fn // sn
+#     print(f"{fn} {op} {sn} = {result}")
+# elif op == "%":
+#     result = fn % sn
+#     print(f"{fn} {op} {sn} = {result}")
+# elif op == "**":
+#     result = fn ** sn
+#     print(f"{fn} {op} {sn} = {result}")
+# elif op == "all":
+#     print(f"{fn} + {sn} = {fn + sn}")
+#     print(f"{fn} - {sn} = {fn - sn}")
+#     print(f"{fn} * {sn} = {fn * sn}")
+#     print(f"{fn} / {sn} = {fn / sn}")
+#     print(f"{fn} // {sn} = {fn // sn}")
+#     print(f"{fn} % {sn} = {fn % sn}")
+#     print(f"{fn} ** {sn} = {fn ** sn}")
+# else:
+#     print("Enter valid value")
+    
+
